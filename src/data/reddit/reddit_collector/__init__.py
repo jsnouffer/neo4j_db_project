438bc6e0e9879abd50e8a207b35995ad73fd83d0
Jason Snouffer <jsnouff@gmail.com>
2021-03-06 21:26:47 +0000

    Network experiment

--- src/data/reddit/reddit_collector/__init__.py
+++ src/data/reddit/reddit_collector/__init__.py
@@ -4,6 +4,9 @@ import os
 import yaml
 from dotenv import load_dotenv
 
+from reddit_collector.network import RedditNetwork
+from reddit_collector.relationships import Comments, CommentsReplies, Submissions
+
 load_dotenv(os.getenv('CONFIG_ENV'))
 
 try:
--- src/data/reddit/reddit_collector/__main__.py
+++ src/data/reddit/reddit_collector/__main__.py
@@ -5,6 +5,11 @@ import sys
 from praw import Reddit
 from reddit_collector.redis import InputQueue
 
+from neo4j import GraphDatabase
+
+from reddit_collector import RedditNetwork, Comments
+from reddit_collector.data_models import Redditor
+
 def main():
     logger = logging.getLogger("main")
     logger.debug(os.environ.values())
@@ -16,16 +21,32 @@ def main():
         user_agent='my user agent')
     logger.info("connected to Reddit")
 
-    queue = InputQueue()
+    # Create driver instance
+    driver = GraphDatabase.driver(
+        "bolt://localhost:7687", auth=("neo4j", "bitnami")
+    )
 
-    sr = reddit.subreddit('all')
-    for comment in sr.stream.comments():
-        print(dir(comment))
-        break
-        # queue.comment_queue.enqueue('reddit_collector.db.insert_comment', comment)
-        # queue.redditor_queue.enqueue('reddit_collector.db.insert_redditor', comment.author)
-        # queue.submission_queue.enqueue('reddit_collector.db.insert_submission', comment.submission)
-        # queue.subreddit_queue.enqueue('reddit_collector.db.insert_subreddit', comment.submission.subreddit)
+    net = RedditNetwork(
+        driver=driver,
+        components=[
+            # Other relationship types are Submissions and CommentsReplies
+            # Other data models available as components are Subreddit and Submission
+            Comments(Redditor(reddit, "BloodMooseSquirrel", limit=5)),
+            Comments(Redditor(reddit, "Anub_Rekhan", limit=5))
+        ]
+    )
+    # net.create_constraints()
+    net.run_cypher_code()
+    # net.add_karma(reddit)
+
+    # queue = InputQueue()
+
+    # sr = reddit.subreddit('all')
+    # for comment in sr.stream.comments():
+    #     queue.comment_queue.enqueue('reddit_collector.db.insert_comment', comment)
+    #     queue.redditor_queue.enqueue('reddit_collector.db.insert_redditor', comment.author)
+    #     queue.submission_queue.enqueue('reddit_collector.db.insert_submission', comment.submission)
+    #     queue.subreddit_queue.enqueue('reddit_collector.db.insert_subreddit', comment.submission.subreddit)
 
 if __name__ == "__main__":
     sys.exit(main()) 
