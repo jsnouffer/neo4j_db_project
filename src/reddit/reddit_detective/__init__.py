7748c99b91d005012ad09cb72714f552ebaa28d9
Jason Snouffer <jsnouff@gmail.com>
2021-04-06 15:10:49 +0000

    Removed reddit_detective module

--- src/reddit/reddit_detective/__init__.py
+++
@@ -1,4 +0,0 @@
-from reddit_detective.network import RedditNetwork
-from reddit_detective.relationships import Comments, CommentsReplies, Submissions
-
-VERSION = "0.1.4"
--- src/reddit/reddit_detective/utils.py
+++
@@ -1,5 +0,0 @@
-def strip_punc(str_):
-    str_ = str_.replace("\'", "")
-    str_ = str_.replace("\"", "")
-    str_ = str_.replace("\\", "")
-    return str_
