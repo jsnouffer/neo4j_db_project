abbd87eeedf88ba350d9343b6af17375c0f68db2
speedcoder5 <tellronneely@gmail.com>
2021-04-18 19:49:32 +0000

    add neomodel test case

--- src/data/hn/hn_collector/__init__.py
+++ src/data/hn/hn_collector/__init__.py
@@ -10,5 +10,6 @@ try:
     with open('logging.yaml', 'rt') as stream:
         config = yaml.load(stream, Loader=yaml.FullLoader)
     logging.config.dictConfig(config)
+    print('Setup logging per ' + str(config))
 except FileNotFoundError:
-    print("No valid logging file found")
+    print('No valid logging file found')
--- src/data/hn/hn_collector/__main__.py
+++ src/data/hn/hn_collector/__main__.py
@@ -2,26 +2,65 @@ import logging
 import os
 import sys
 
-
 from neo4j import GraphDatabase
+from neomodel import StructuredNode, StringProperty, RelationshipTo, RelationshipFrom, config
+
+## neo4j
+class HelloWorldExample:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def print_greeting(self, message):
+        with self.driver.session() as session:
+            greeting = session.write_transaction(self._create_and_return_greeting, message)
+            print(greeting)
+
+    @staticmethod
+    def _create_and_return_greeting(tx, message):
+        result = tx.run("CREATE (a:Greeting) "
+                        "SET a.message = $message "
+                        "RETURN a.message + ', from node ' + id(a)", message=message)
+        return result.single()[0]
+
+# neomodel
+class Book(StructuredNode):
+    title = StringProperty(unique_index=True)
+    author = RelationshipTo('Author', 'AUTHOR')
+
+class Author(StructuredNode):
+    name = StringProperty(unique_index=True)
+    books = RelationshipFrom('Book', 'AUTHOR')
 
 
 def main():
-    logger = logging.getLogger("main")
-    logger.debug(os.environ.values())
-    logger.debug("----------------------------------------------")
+    print("hello")
+    print(os.environ.values())
+    print("----------------------------------------------")
 
-    logger.debug("connecting to Reddit")
-    reddit = Reddit(client_id='Cz8OU1vxajnWDw',
-        client_secret='5qax29ZPI2_Rdjc1TsXXEypFduk',
-        user_agent='my user agent')
-    logger.info("connected to Reddit")
+    print("connecting to hn")
 
     # Create driver instance
+    print("connecting to neo4j")
     driver = GraphDatabase.driver(
         "bolt://localhost:17687", auth=("neo4j", "bitnami")
     )
+    print("wrinting hello message")
+    greeter = HelloWorldExample(driver)
+    greeter.print_greeting('hello neo4j')
+    driver.close()
 
+    print("closed to neo4j")
+
+    print("using neomodel")
+    config.DATABASE_URL = 'bolt://neo4j:bitnami@localhost:17687'
+    print("adding Harry Potter")
+
+    harry_potter = Book(title='Harry potter and the..').save()
+    rowling =  Author(name='J. K. Rowling').save()
+    harry_potter.author.connect(rowling)
+
+    print("added Harry Potter")
 
 if __name__ == "__main__":
     sys.exit(main()) 
